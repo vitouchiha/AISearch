63ed6a8a23754f00e40cfca6fad79f852ed3077a
Ian Gutjahr <ian@ian.rocks>
2025-02-11 19:03:01 +0000

    Added esm imports for consistancy

--- config/deps.ts
+++ config/deps.ts
@@ -7,12 +7,11 @@ export {
   } from "https://deno.land/x/oak@v12.6.1/mod.ts";
   
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-export { Ratelimit } from "npm:@upstash/ratelimit@latest";
+export { Ratelimit } from "https://esm.sh/@upstash/ratelimit@latest";
 export { Redis } from "https://esm.sh/@upstash/redis";
 
 export { SemanticCache } from "https://esm.sh/@upstash/semantic-cache";
 export { Index } from "https://esm.sh/@upstash/vector";
 export { generateText } from "https://esm.sh/ai";
 export { createGoogleGenerativeAI } from "https://esm.sh/@ai-sdk/google";
-
-export { Filter } from "npm:bad-words";
+export { Filter } from "https://esm.sh/bad-words";
